a7c5f0594b494a73e2f9f8d02165466622e6448a
Carlos Lobo Mata <carlos.lobomata@telefonica.com>
2018-11-26 23:36:05 +0100

    start to move towards a service

--- src/app/services/music-loader.service.ts
+++ src/app/services/music-loader.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, Observable, of, throwError, forkJoin } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MusicLoaderService {
+  musicLoader = new Subject();
+  music$ = this.musicLoader.asObservable();
+  music: File[] = [];
   deckLoader = [new Subject(), new Subject()];
-  deck$ = [this.deckLoader[0].asObservable(), this.deckLoader[1].asObservable()];
-  deck: File[] = [null, null];
+  decksongs$ = [this.deckLoader[0].asObservable(), this.deckLoader[1].asObservable()];
+  decksongs: File[] = [null, null];
 
-  load(deck, wavesurfer) {
-    this.deck[deck] = wavesurfer;
-    this.deckLoader[deck].next(this.deck[deck]);
+  load(deck, song) {
+    this.decksongs[deck] = song;
+    this.deckLoader[deck].next(this.decksongs[deck]);
+  }
+  addSong(song: File) {
+    this.music.push(song);
+    this.musicLoader.next(this.music);
+  }
+  deleteSong(song) {
+    this.music.splice(this.music.indexOf(song), 1);
+    this.musicLoader.next(this.music);
   }
 }
--- src/app/services/player-service.service.ts
+++ src/app/services/player-service.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerServiceService {
-  constructor() {}
+  deckLoader = [new Subject(), new Subject()];
+  deck$ = [this.deckLoader[0].asObservable(), this.deckLoader[1].asObservable()];
+  deck: File[] = [null, null];
+
+  load(deck, wavesurfer) {
+    this.deck[deck] = wavesurfer;
+    this.deckLoader[deck].next(this.deck[deck]);
+  }
 }
