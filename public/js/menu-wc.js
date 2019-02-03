50ff7a361000831fa0104dd4e29ae89fa0f6c2c4
Carlos Lobo Mata <carlos.lobomata@telefonica.com>
2019-02-03 17:30:31 +0100

    fix: show loading message during song load

--- public/js/menu-wc.js
+++ public/js/menu-wc.js
@@ -57,13 +57,13 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                         <a href="modules/AppModule.html" data-type="entity-link">AppModule</a>
                             <li class="chapter inner">
                                 <div class="simple menu-toggler" data-toggle="collapse"
-                                    ${ isNormalMode ? 'data-target="#components-links-module-AppModule-9dfa3c68139eae6eddd3781f7bbbdb08"' : 'data-target="#xs-components-links-module-AppModule-9dfa3c68139eae6eddd3781f7bbbdb08"' }>
+                                    ${ isNormalMode ? 'data-target="#components-links-module-AppModule-2d5ccd27a8a50a1269349970d07165e2"' : 'data-target="#xs-components-links-module-AppModule-2d5ccd27a8a50a1269349970d07165e2"' }>
                                     <span class="icon ion-md-cog"></span>
                                     <span>Components</span>
                                     <span class="icon ion-ios-arrow-down"></span>
                                 </div>
                                 <ul class="links collapse"
-                                    ${ isNormalMode ? 'id="components-links-module-AppModule-9dfa3c68139eae6eddd3781f7bbbdb08"' : 'id="xs-components-links-module-AppModule-9dfa3c68139eae6eddd3781f7bbbdb08"' }>
+                                    ${ isNormalMode ? 'id="components-links-module-AppModule-2d5ccd27a8a50a1269349970d07165e2"' : 'id="xs-components-links-module-AppModule-2d5ccd27a8a50a1269349970d07165e2"' }>
                                         <li class="link">
                                             <a href="components/AppAboutComponent.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules">AppAboutComponent</a>
                                         </li>
