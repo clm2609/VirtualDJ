fd924da04ce4ec44c66ec86e716eb1f8d0833b5a
Carlos Lobo <karlos.ogame@gmail.com>
2019-02-23 20:48:01 +0000

    Merge branch 'feat/fixes-and-tests' into 'develop'
    Feat/fixes and tests
    
    See merge request clm2609/tfg!26

--- src/app/components/app-tabs/app-tabs.component.spec.ts
+++ src/app/components/app-tabs/app-tabs.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppTabsComponent } from './app-tabs.component';
+import { AppModule } from 'src/app/app.module';
 
 describe('AppTabsComponent', () => {
   let component: AppTabsComponent;
@@ -8,7 +9,7 @@ describe('AppTabsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AppTabsComponent]
+      imports: [AppModule]
     }).compileComponents();
   }));
 
